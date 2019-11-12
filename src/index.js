033536f99efdbb8cf7925b31f993f83c2adee5bd
Felix <felixaviermartinez@gmail.com>
2019-11-12 16:38:51 -0500

    lots of css changes, and a show page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+let cardShowView = false;
 let characterUrl = 'http://localhost:3000/characters'
 let addCharacter = false
 const addBtn = document.querySelector('#new-character-btn')
 const characterForm = document.querySelector('.container')
+const cardViewContainer = document.querySelector(".show-card-container")
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -20,10 +21,20 @@ function toggleFormView() {
   }
 }
 
+function togglecardShowView() {
+  console.log("this runs too")
+  cardShowView = !cardShowView
+  if(cardShowView) {
+    cardViewContainer.classList.add("visible")
+  } else {
+    cardViewContainer.classList.remove("visible")
+  }
+}
+
 let form = document.getElementsByClassName('add-character-form')[0]
 form.addEventListener('submit', (e)=> {
   submitData(form.name.value, form.image.value)
-  console.log(`${form.name.value} ${form.image.value}`)
+  // console.log(`${form.name.value} ${form.image.value}`)
   form.reset()
   // let index = document.getElementById('character-collection')
   // index.innerHTML = ''
@@ -48,12 +59,17 @@ function submitData(name, image){
   .then(json => renderACharacter(json))
 }
 
-function getCharacters() {
-  fetch(characterUrl)
-  .then(resp => resp.json())
-  .then(data => {renderCharacters(data)})
-}
+// function getCharacters() {
+//   fetch(characterUrl)
+//   .then(resp => resp.json())
+//   .then(data => {renderCharacters(data)})
+// }
 
+function getCharacters(url) {
+  fetch(url)
+  .then(resp => resp.json())
+  .then(char => renderCharacters(char))
+}
 
 function renderACharacter(char){
   div.innerHTML += `
@@ -61,10 +77,12 @@ function renderACharacter(char){
   <h2>${char.name}</h2>
   <img src=${char.image} class="character-avatar" />
   <p> ${char.likes} </p>
+  <p style="display:inline-block; vertical-align: middle;">
   <button class="like-btn" data-action="like">Like</button>
   <button class="delete-btn" data-action="delete">Delete</button>
+  </p>
   </div>`
-}
+} // added p tag to buttons for the inline block
 
 div = document.getElementById('character-collection')
 
@@ -105,14 +123,14 @@ function renderCharacters(characters) {
   
   document.addEventListener('click', event => {
     if (event.target.dataset.action === 'like') {
-      let parent = event.target.parentNode
+      let parent = event.target.parentNode.parentNode
       let parentUrl = parent.dataset.url
       let parentId = parent.dataset.id
-      let likeCount = event.target.previousSibling.previousSibling
+      let likeCount = event.target.parentNode.previousSibling.previousSibling // added parentNode to line
       likeCount.innerText++
       incrementLikes(parentId, parentUrl, likeCount)
     } else if (event.target.dataset.action === 'delete') {
-      let parent = event.target.parentNode
+      let parent = event.target.parentNode.parentNode
       let parentUrl = parent.dataset.url
       let parentId = parent.dataset.id
       deleteData(parentId, parentUrl)
@@ -120,16 +138,41 @@ function renderCharacters(characters) {
     }
   })
 
-  document.addEventListener('click', e => {
-    if (e.target.classList[0] === 'character-avatar') {
-      console.log('clearing the index')
-      let index = document.getElementById('character-collection')
-      index.innerHTML = ''
+  let showDiv = document.getElementsByClassName('show-cards')[0]
 
+  function renderShowCharacter(char){
+    showDiv.innerHTML += `
+    <div class="s-card show-card" data-url=${characterUrl} data-id=${char.id}>
+    <h2>${char.name}</h2>
+    <img src=${char.image} class="" />
+    <p> ${char.likes} </p>
+    <p style="display:inline-block; vertical-align: middle;">
+    <button class="like-btn" data-action="like">Like</button>
+    <button class="delete-btn" data-action="delete">Delete</button>
+    </p>
+    </div>`
+  } // added p tag to buttons for the inline block
+
+  document.addEventListener('click', e => {
+    if (e.target.className == 'character-avatar') {
+      // console.log(e.target.parentNode.dataset.id)
+      let charId = e.target.parentNode.dataset.id
+      fetch(`${characterUrl}/${charId}`)
+        .then(resp => resp.json())
+        .then(data => renderShowCharacter(data))
+      togglecardShowView()
+
+    }
+
+    if(e.target.classList[0] == 'show-cards') {
+      // console.log('click click clickckckckc')
+      // console.log(showDiv)
+      showDiv.innerHTML = ''
+      togglecardShowView()
     }
   })
   
-  getCharacters()
+  getCharacters(characterUrl)
   
 })
 
